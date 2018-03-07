4f51ec56e7f8eb3551be547127580dbf11ef6a0d
Michal Polovka <miskopo@bowmaster.localdomain>
2018-03-07 16:06:37 +0100

    Initial commit

---
+++ src/main/java/cz/muni/fi/pv168/lcars.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv168;
+
+import javax.swing.*;
+
+/**
+ * @author Michal Polovka
+ */
+public class lcars {
+    private JFormattedTextField STARFLEETPERSONELMANAGERFormattedTextField;
+    private JPanel panel1;
+}
