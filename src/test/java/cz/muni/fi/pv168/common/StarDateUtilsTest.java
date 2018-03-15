5a2f114814c717fc5752eca1bcc7c2c2b06772d6
Michal Polovka <michal.polovka@gmail.com>
2018-03-15 10:44:39 +0100

    Added more tests for star date converter

--- src/test/java/cz/muni/fi/pv168/common/StarDateUtilsTest.java
+++ src/test/java/cz/muni/fi/pv168/common/StarDateUtilsTest.java
@@ -14,5 +14,12 @@ public class StarDateUtilsTest {
     public void dateToStarDate() {
         LocalDate isoDate = LocalDate.of(2019, 3, 15);
         assertTrue(StarDateUtils.dateToStarDate(isoDate) == 11222.15);
+
+
+        isoDate = LocalDate.of(1526, 1, 1);
+        assertTrue(StarDateUtils.dateToStarDate(isoDate) == -41003.01);
+
+        isoDate = LocalDate.of(2459, 12, 31);
+        assertTrue(StarDateUtils.dateToStarDate(isoDate) == 54096.31);
     }
 }
