d80f45b93ce0e5532bd82ef358df4b8828f82931
Katarina Bulkova <xbulkova@fi.muni.cz>
2018-03-20 13:13:45 +0100

    All interfaces done

--- src/main/java/cz/muni/fi/pv168/model/AssignmentManager.java
+++ src/main/java/cz/muni/fi/pv168/model/AssignmentManager.java
@@ -1,5 +1,9 @@
 package cz.muni.fi.pv168.model;
 
+import cz.muni.fi.pv168.common.IllegalEntityException;
+import cz.muni.fi.pv168.common.ServiceFailureException;
+import cz.muni.fi.pv168.common.ValidationException;
+
 import java.util.List;
 
 /**
@@ -7,17 +11,73 @@ import java.util.List;
  */
 public interface AssignmentManager {
 
-    void createAssignment(Assignment assignment);
+    /**
+     * Stores new assignment into the database.
+     *
+     * @param assignment assignment to be added to the database
+     * @throws IllegalArgumentException when assignment is null
+     * @throws ServiceFailureException when db operation fails
+     * @throws ValidationException when crewman breaks validation rules
+     * (ship or crewman is null)
+     * @throws IllegalEntityException ???
+     */
+    void createAssignment(Assignment assignment) throws ServiceFailureException, ValidationException, IllegalEntityException;
 
-    Assignment getAssignmentById(Long id);
+    /**
+     * Returns assignment by given Id
+     *
+     * @param id primary key of requested assignment
+     * @return assignment by given Id
+     * @throws IllegalArgumentException when given id is null
+     * @throws ServiceFailureException when db operation fails
+     */
+    Assignment getAssignmentById(Long id) throws ServiceFailureException;
 
-    List<Assignment> findAllAssignments();
+    /**
+     * Returns list of all assignments
+     * @return list of all assignments
+     * @throws ServiceFailureException when db operation fails
+     */
+    List<Assignment> findAllAssignments() throws ServiceFailureException;
 
-    void updateAssignment(Assignment assignment);
+    /**
+     * Updates assignment in the database.
+     *
+     * @param assignment updated assignment to be stored into the database
+     * @throws IllegalArgumentException when assignment is null
+     * @throws ServiceFailureException when db operation fails
+     * @throws ValidationException when assignment breaks validation rules
+     * (ship, crewman or startdate is null)
+     * @throws IllegalEntityException when assignment does not exist in the databse
+     */
+    void updateAssignment(Assignment assignment) throws ServiceFailureException, ValidationException, IllegalEntityException;
 
-    void deleteAssignment(Assignment assignment);
+    /**
+     * Deletes assignment form the database
+     *
+     * @param assignment assignment to be deleted from the db
+     * @throws IllegalArgumentException when assignment is null
+     * @throws ServiceFailureException when db operation fails
+     * @throws IllegalEntityException when assignment does not exist in the database
+     */
+    void deleteAssignment(Assignment assignment) throws ServiceFailureException, IllegalEntityException;
 
-    List<Assignment> findAssignmentByShip(Ship ship);
+    /**
+     * Returns list of all assigments of given ship
+     * @param ship key of requested assignment
+     * @throws ServiceFailureException when db operation fails
+     * @throws IllegalArgumentException when ship is null
+     * @return list of all assignments of given ship
+     */
+    List<Assignment> findAssignmentByShip(Ship ship) throws ServiceFailureException;
 
-    List<Assignment> findAssignmentByCrewman(Crewman crewman);
+    /**
+     * Returns list of all assignemnts of given crewman
+     *
+     * @param crewman key of requested assignment
+     * @throws ServiceFailureException when db operation fails
+     *@throws IllegalArgumentException when crewman is null
+     * @return list of all assignemnts of gien crewman
+     */
+    List<Assignment> findAssignmentByCrewman(Crewman crewman) throws ServiceFailureException;
 }
