c9815b9ba97b581026357581cda829e655c3f940
Michal Polovka <michal.polovka@gmail.com>
2018-04-04 19:51:23 +0200

    WIP SQL db

---
+++ src/main/java/cz/muni/fi/pv168/common/DBUtils.java
@@ -0,0 +1,69 @@
+package cz.muni.fi.pv168.common;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * Some DB tools.
+ *
+ * @author Petr Adamek
+ * @author Martin Kuba
+ * @author Michal Polovka
+ * */
+public class DBUtils {
+
+    /**
+     * Extract key from ResultSet got from PreparedStatement.getGeneratedKeys().
+     *
+     * @param key resultSet with key
+     * @return key from given result set
+     * @throws SQLException when operation fails
+     */
+    public static Long getId(ResultSet key) throws SQLException {
+        if (key.getMetaData().getColumnCount() != 1) {
+            throw new IllegalArgumentException("Given ResultSet contains more columns");
+        }
+        if (key.next()) {
+            Long result = key.getLong(1);
+            if (key.next()) {
+                throw new IllegalArgumentException("Given ResultSet contains more rows");
+            }
+            return result;
+        } else {
+            throw new IllegalArgumentException("Given ResultSet contains no rows");
+        }
+    }
+
+    /**
+     * Executes SQL script.
+     *
+     * @param dataSource datasource
+     * @param is InputStream obtained by getClass().getResourceAsStream()
+     * @throws SQLException when operation fails
+     */
+    public static void executeSqlScript(DataSource dataSource, InputStream is) throws SQLException, IOException {
+        try (Connection c = dataSource.getConnection()) {
+            Scanner s = new Scanner(is).useDelimiter(";");
+            while (s.hasNext()) {
+                String sql = s.next().trim();
+                if (sql.isEmpty()) continue;
+                try (PreparedStatement st1 = c.prepareStatement(sql)) {
+                    st1.execute();
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static void createDatabase(DataSource dataSource)
+
+}
--- src/main/java/cz/muni/fi/pv168/model/ShipManagerImpl.java
+++ src/main/java/cz/muni/fi/pv168/model/ShipManagerImpl.java
@@ -4,12 +4,22 @@ import cz.muni.fi.pv168.common.IllegalEntityException;
 import cz.muni.fi.pv168.common.ServiceFailureException;
 import cz.muni.fi.pv168.common.ValidationException;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * @author Michal Polovka
  */
 public class ShipManagerImpl implements ShipManager {
+
+    private DataSource dataSource;
+
+    @SuppressWarnings("WeakerAccess")
+    public ShipManagerImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Override
     public void createShip(Ship ship) throws ServiceFailureException, ValidationException, IllegalEntityException {
         throw new UnsupportedOperationException();
